4d23bb0bfd53ac6f603602493a389b3c09333a7e
曹凱嵐Jacky <kaij1941@gmail.com>
2022-04-13 21:41:39 +0800

    feat: 增加 patchTodo功能

---
+++ patchTodo.js
@@ -0,0 +1,41 @@
+const errorHandle = require('./errorHandle');
+const { headers } = require('./libs');
+
+function patchTodo(req, res, todos) {
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk;
+  })
+  req.on('end', () => {
+    try {
+        const title = JSON.parse(body).title;
+        const id = req.url.split('/')[2];
+
+        const index =todos.find(element=>element.id = id)
+        
+        //沒正確傳入title
+        if (!title) {
+            errorHandle(res);
+            return;
+        }
+
+        //找不到此Id
+        if (index==-1) {
+            errorHandle(res);
+            return;
+        }
+
+        todos[index].title= title;
+
+        res.writeHead(200, headers);
+        res.write(JSON.stringify({
+          status: 'success',
+          data: todos
+        }));
+    } catch {
+      errorHandle(res);
+    }
+  })
+}
+
+module.exports = patchTodo; 
